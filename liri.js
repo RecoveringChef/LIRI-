3fefb7c58889261ca5ce8c87203a7b72d356dd4f
RecoveringChef <54086786+RecoveringChef@users.noreply.github.com>
2019-09-26 10:37:43 -0400

    spotify working, omdb started

--- liri.js
+++ liri.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
 var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+
 var axios = require("axios");
 var moment = require("moment");
 var fs = require("fs");
@@ -18,44 +19,57 @@ function spotifyThis(input){
     spotify
         .search({ type: 'track', query: input, limit: 1})
         .then(function(response) {
-                console.log(JSON.stringify(response.tracks.items[0].artist,name, null, 2))
-            }
-          )
+                console.log(JSON.stringify(response.tracks.items[0].artists[0].name, null, 2))
+                console.log(JSON.stringify(response.tracks.items[0].name, null, 2))
+                console.log(JSON.stringify(response.tracks.items[0].album.name, null, 2))
+                console.log(JSON.stringify(response.tracks.items[0].artists[0].external_urls.spotify , null, 2))
+            })
         
         .catch(function (err){
         console.log(err);           
         });
-};
+}
 
 
 function concertThis(){
     
 }
 function movieThis(){
+    axios.get("http://www.omdbapi.com/?apikey=trilogy&s=" + userInput)
+  .then(function (response) {
+    // handle success
+    console.log(response.data.Search[0]);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+ 
+    
     
 }
 function doWhatItSays(){
     
 }
-/*
-switch(){
-    case "spotify-this-song":
-        spotifyThis();
+
+switch(whatToDo){
+    case "spotify-this":
+        spotifyThis(userInput);
         break;
-}
-switch(){
+
+
     case "movie-this":
-        concertThis();
+        movieThis();
         break;
-}
-switch(){
+
     case "concert-this":
         movieThis();
         break;
-}
-switch(){
+
     case "do-what-it-says":
        doWhatItSays();
         break;
 }
-*/
