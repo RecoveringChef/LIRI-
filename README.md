1b1109eda2a8145c062c345e9fcf54be03f8b400
RecoveringChef <54086786+RecoveringChef@users.noreply.github.com>
2019-10-01 01:46:57 -0400

    functions working, read me started. need some screen caps to add

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# LIRI-
-first node.js project. LIRI will pull from API's to find you info
+# Meet LIRI #
+## AKA: Line Item Recongnition Interface ##
+
+**Objective**
+
+LIRI takes in commands from the terminal and will look up song data, movie info, and concerts and then pass then to you in the terminal
+
+**Technologies and Tools Used**
+
+-node.js wiht axios, moment, and Spotify packages
+
+-Bands in Town, and OMDB API's
+
+**Commands**
+
+*movie-this* +  a movie title will return a synopsys of that movie
+
+*spotify-this* + a song name will yield details about that song
+
+*concert-this* + a band name will give info on that artist's upcoming shows
+
+*do-what-it-says* will take in the argument from a linked text file and output the results of that instead of termnal values
+
--- liri.js
+++ liri.js
@@ -1,3 +1,5 @@
+
+// keys and modules
 require("dotenv").config();
 
 var keys = require("./keys.js");
@@ -8,13 +10,11 @@ var axios = require("axios");
 var moment = require("moment");
 var fs = require("fs");
 
-
-
-
-
+//global variables - AKA user inputs
 var whatToDo = process.argv[2];
 var userInput = process.argv.slice(3).join(" ");
 
+//spotify func to call for song/artist data and display what we want out of it
 function spotifyThis(input){
     spotify
         .search({ type: 'track', query: input, limit: 4})
@@ -28,38 +28,77 @@ function spotifyThis(input){
         .catch(function (err){
         console.log(err);           
         });
-}
+};
 
-
-function concertThis(){
-    
-}
+//movie func to call omdb for movie data and display what we want out of it
 function movieThis(){
-    axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + userInput + "&y=&plot=short")
-  .then(function (response) {
-    //console.log(response.data)
-    console.log("Title: " + response.data.Title);
-    console.log("Released:  "+ response.data.Year);
-    console.log("IMdB Rating: " + response.data.imdbRating);
-    console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-    console.log("Country: " + response.data.Country);
-    console.log("Language: " + response.data.Language);
-    console.log("Plot: " + response.data.Plot);
-    console.log("Actors: " + response.data.Actors);
+  axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + userInput + "&y=&plot=short")
+.then(function (response) {
+  //console.log(response.data)
+  console.log("Title: " + response.data.Title);
+  console.log("Released:  "+ response.data.Year);
+  console.log("IMdB Rating: " + response.data.imdbRating);
+  console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+  console.log("Country: " + response.data.Country);
+  console.log("Language: " + response.data.Language);
+  console.log("Plot: " + response.data.Plot);
+  console.log("Actors: " + response.data.Actors);
 
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });    
+})
+.catch(function (error) {
+  // handle error
+  console.log(error);
+})
+.finally(function () {
+  // always executed
+});    
+};
+
+//bands in town call to get show info and display what we want
+function concertThis(){
+  axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp")
+  .then(function (response){
+//console.log(response.data);
+
+
+if(response.data.length == 0){
+  console.log("Nothing happening. Try again later, maybe?")
 }
+  
+else {
+
+  for(var i = 0; i < response.data.length; i++){
+    var concertData =[
+  "Bands: "+ response.data[i].lineup,
+  "Venue: " + response.data[i].venue.name,
+  "Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region,  
+  "Date: " + moment(response.data[i].datetime, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYY, h:mm A'),
+  "   "
+].join("\n");
+   console.log(concertData)
+  }
+  
+  }
+  
+})
+}
+
+//func to take inoput from linked text file and perform that as a command in place of argv
 function doWhatItSays(){
-    
-}
+  fs.readFile("random.txt", "utf8", function(err, data){
+      if(err){
+          console.log(err)
+      } 
+      
+      else {
+        var fromTxt = data.split(",")
+        
+      };
+    }
+  )
+  }
 
+  //switch to tell what functions to call based on user input argv[2]
 switch(whatToDo){
     case "spotify-this":
         spotifyThis(userInput);
@@ -67,14 +106,14 @@ switch(whatToDo){
 
 
     case "movie-this":
-        movieThis();
+        movieThis(userInput);
         break;
 
-//    case "concert-this":
-  //      concertThis();
-  //      break;
+    case "concert-this":
+        concertThis(userInput);
+        break;
 
-  //  case "do-what-it-says":
- //      doWhatItSays();
-  //      break;
+    case "do-what-it-says":
+      doWhatItSays();
+       break;
 }
